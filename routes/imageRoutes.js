b34402f7e5a6de0a0ed9efdd2d83bedb9c212259
Himanshu Rawat <156574192+Himanshu003-r@users.noreply.github.com>
2025-10-08 11:38:10 +0530

    Delete routes directory

--- routes/imageRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const router = Router()
-import { upload } from "../middleware/multer.js";
-import { uploadImage } from "../controllers/imageController.js";
-import { verifyJWT } from "../middleware/authMiddleware.js";
-
-router.post('/upload-image',verifyJWT,upload.single('image'),uploadImage)
-
-export default router
