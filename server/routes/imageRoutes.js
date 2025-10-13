e7bbcdba0a00f87f7ae6e955963f4e2edbfe9b57
Himanshu Rawat <himanshur651@gmail.com>
2025-10-13 13:33:26 +0530

    Updated imageRoute

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 const router = Router()
 import { upload } from "../middleware/multer.js";
-import { uploadImage } from "../controllers/imageController.js";
+import { getSingleImage, updateImage, uploadImage } from "../controllers/imageController.js";
 import { verifyJWT } from "../middleware/authMiddleware.js";
 
 router.post('/upload-image',verifyJWT,upload.single('image'),uploadImage)
+router.patch('/update-image/:id',verifyJWT,upload.single('image'),updateImage)
+router.get('/singleImage/:id',getSingleImage)
 
 export default router
